b85fb8bc195163e0f007780cf49afb105190114d
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2023-12-29 20:10:07 +0530

    memory game finished page

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk0Fat3MBmTir8kFlSExkAV21XpTqReLs",
+  authDomain: "memory-game-8551a.firebaseapp.com",
+  projectId: "memory-game-8551a",
+  storageBucket: "memory-game-8551a.appspot.com",
+  messagingSenderId: "248529230038",
+  appId: "1:248529230038:web:d00d888f2c932842269177",
+};
+
+//init firebase
+initializeApp(firebaseConfig);
